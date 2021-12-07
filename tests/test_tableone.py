7e901109b870933b2e7d245219a4a6eba479bcb2
Henry Mitchell <hmitchell4@bwh.harvard.edu>
2021-12-07 11:41:40 -0500

    Improved coverage
    Closes #1

--- tests/test_tableone.py
+++ tests/test_tableone.py
@@ -575,13 +575,16 @@ class TestTableOne(unittest.TestCase):
                                   groupings=["Col1", "Col2"])
         assert_frame_equal(table.counts().fillna(""), expect)
 
-    def test_booleanize_no_fillna(self):
         df = pd.read_csv(os.path.join(test_path, "booleanize_expect.csv"))
         expect = pd.Series([False, True, np.nan, True])
         for col in df.columns:
             with self.subTest(col=col):
                 expect.name = col
                 assert_series_equal(booleanize(df[col]), expect)
+
+        col = pd.Series(["Value1", "Value2", "Value1"], dtype="category")
+        with self.assertRaises(ValueError):
+            booleanize(col)
 
     def test_booleanize__fillna(self):
         df = pd.read_csv(os.path.join(test_path, "booleanize_expect.csv"))
